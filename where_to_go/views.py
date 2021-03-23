5520ffe026bd68a399fb2d1f6a52f598d51e0386
balancy <balancy@gmail.com>
2021-03-23 12:52:20 +0100

    Add places app

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 
-def show_blank_page(request):
-    return render(request, 'blank.html')
-
-
 def show_index(request):
     return render(request, 'index.html')
