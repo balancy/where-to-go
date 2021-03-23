fe43c4d35b0f96fc9fa047ac0d1a686562793f5b
balancy <balancy@gmail.com>
2021-03-23 16:40:27 +0100

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import GeoJson
+from .models import GeoJson, PlaceImage
 
 # Register your models here.
 admin.site.register(GeoJson)
+admin.site.register(PlaceImage)
--- places/models.py
+++ places/models.py
@@ -10,3 +10,11 @@ class GeoJson(models.Model):
 
     def __str__(self):
         return f"{self.__class__.__name__}<{self.title}>"
+
+
+class PlaceImage(models.Model):
+    path = models.CharField(max_length=200)
+    geojson = models.ForeignKey(GeoJson, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}<{self.path}>"
