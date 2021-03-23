9a171881d4e940795e0e2613751feac88d14056e
balancy <balancy@gmail.com>
2021-03-23 13:38:06 +0100

    Add GeoJson model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import GeoJson
+
 # Register your models here.
+admin.site.register(GeoJson)
--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-print("Hello")
+
+class GeoJson(models.Model):
+    title = models.CharField(max_length=100)
+    description_short = models.TextField(null=True)
+    description_long = models.TextField(null=True)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.__class__.__name__}<{self.title}>"
