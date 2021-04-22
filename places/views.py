15963d6cfc47e9ba48714de069d88b4cd5869fbb
balancy <balancy@gmail.com>
2021-04-22 18:58:29 +0200

    Return JsonResponse when we ask for place details

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
 
 from .models import GeoJson
 
@@ -21,9 +22,32 @@ def serialize_geojson(geojson):
 def show_index(request):
     features = [serialize_geojson(geojson) for geojson in GeoJson.objects.all()]
 
-    data = {
+    context = {
         "type": "FeatureCollection",
         "features": features,
     }
 
-    return render(request, "index.html", context={"data": data})
+    return render(request, "index.html", context={"places": context})
+
+
+def show_place_detail(request, place_id):
+    place = get_object_or_404(GeoJson, placeId=place_id)
+
+    context = {
+        "title": place.title,
+        "imgs": [elm.image.url for elm in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.latitude,
+            "lng": place.longitude,
+        }
+    }
+
+    return JsonResponse(
+        context,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
+        }
+    )
