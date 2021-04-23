2015ff0d1e4595162036b85099a32b0c3e54b290
balancy <balancy@gmail.com>
2021-04-23 10:43:47 +0200

    Add editing images on GeoJson admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import GeoJson, PlaceImage
 
-# Register your models here.
-admin.site.register(GeoJson)
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
+@admin.register(GeoJson)
+class GeoJsonAdmin(admin.ModelAdmin):
+    inlines = [
+        PlaceImageInline,
+    ]
+
+
 admin.site.register(PlaceImage)
