c90810b3e553eff793cc76184c4abc1cc4bfce41
balancy <balancy@gmail.com>
2021-04-25 14:47:39 +0200

    Prepare web for deploy

--- manage.py
+++ manage.py
@@ -3,8 +3,6 @@
 import os
 import sys
 
-from dotenv import load_dotenv
-
 
 def main():
     """Run administrative tasks."""
@@ -21,5 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    load_dotenv()
     main()
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +26,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env.str("SECRET_KEY", "REPLACE_ME")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# SECURITY WARNINGS
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT")
+SESSION_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 
 # Application definition
@@ -81,7 +94,9 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env.str(
+            'DATABASE_FILEPATH', os.path.join(BASE_DIR, 'db.sqlite3')
+        ),
     }
 }
 
