fe0df40c5ce09e11cd69484cfed6b079be9b6d75
balancy <balancy@gmail.com>
2021-05-03 12:27:35 +0200

    Fix debug variable reading in settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 # SECURITY WARNINGS
 CSRF_COOKIE_SECURE = True
